ea4b57147643cb8883c502c4a20792878a726c55
danieltrusca <truscadani@yahoo.com>
2023-02-03 15:49:14 +0200

    starter project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white w-full h-screen flex justify-center items-center'>App</div>
+  )
+}
+
+export default App
